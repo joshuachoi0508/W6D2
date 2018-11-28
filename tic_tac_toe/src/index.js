ba87c07c039c2af101b85719552f8f5234aedcc0
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-11-27 16:18:19 -0800

    Finish lana del rey themed tic tac toe

--- tic_tac_toe/src/index.js
+++ tic_tac_toe/src/index.js
@@ -1,6 +1,9 @@
 const View = require('./ttt-view.js');// require appropriate file
 const Game = require('./game.js');// require appropriate file
+// const Board = require('./board.js');
 
   $(() => {
     // Your code here
+    const view = new View(new Game(), $('.ttt'));
+    view.setupBoard();
   });
--- tic_tac_toe/src/ttt-view.js
+++ tic_tac_toe/src/ttt-view.js
@@ -1,11 +1,45 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.grid = $el;
+    this.bindEvents();
+  }
 
-  bindEvents() {}
+  bindEvents() {
+    (this.grid).on("click", (el) => {
+      let $square = $(el.target);
+      this.game.playMove($square.data('grid'));
+      this.makeMove($square);
+    });
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
 
-  setupBoard() {}
+    if (this.game.currentPlayer === 'x') {
+      $square.css('background-image', 'url(https://media.tenor.com/images/22bdca75648d345c07d03a470dde74a7/tenor.gif)').css('background-size', '95px');
+    } else {
+      $square.css('background-image', 'url(https://66.media.tumblr.com/8055750f108cf97734f557269f22133b/tumblr_olom1ps6je1uf6f29o1_400.gif)').css('background-size', '95px');
+    }
+
+    if (this.game.isOver()) {
+      alert(`${this.game.winner()} WINS!`);
+    }
+  }
+
+  setupBoard() {
+    let $board = $('<ul></ul>');
+    $board.addClass('board');
+    $('.ttt').append($board);
+
+    for(let i = 0; i < 3; i++) {
+      for(let j = 0; j < 3; j++) {
+        let $sq = $('<li></li>');
+        $sq.addClass('square');
+        $sq.data('grid', [i, j]);
+        $board.append($sq);
+      }
+    }
+  }
 }
 
 module.exports = View;
